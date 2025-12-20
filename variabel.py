ac732e7d3f26aafb820b293c12ae4944f09c9d81
Angelice Novena <belajarcodingangelicenovenaoct@gmail.com>
2025-12-20 20:53:19 +0700

    Menambahkan contoh variabel dasar

--- variabel.py
+++ variabel.py
@@ -1,5 +1,12 @@
-nama = "Angel"
-umur = 16
+# Program variabel dasar Python
+# Contoh Penggunaan variabel
+
+Nama = "Angel"
+Umur + 16
+Kelas = "X TJKT 1"
+Hobi = "Main  Game"
 
 print("Nama:", nama)
 print("Umur:", umur)
+print("Kelas:", kelas)
+print("Hobi:", hobi)
