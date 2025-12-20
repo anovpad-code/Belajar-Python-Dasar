f64d34898056ea1a8bff7b770c5aaee22d491aa5
Angelice Novena <belajarcodingangelicenovenaoct@gmail.com>
2025-12-20 20:41:03 +0700

    Menambahkan contoh Print dasar

--- print_dasar.py
+++ print_dasar.py
@@ -1,3 +1,8 @@
-print("Halo Dunia")
+# Program Print dasar Python
+# Latihan Menampilkan teks ke layar
+
+Print("Hello World")
 print("Saya sedang belajar Python")
 print("Nama saya Angel")
+Print("Ini adalah contoh print dasar")
+
